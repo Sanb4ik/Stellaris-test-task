24b9f77d576eade64414823cd59b20d0bf98ee8a
Sanb4ik <sashabelaz7@gmail.com>
2023-09-12 11:39:41 +0300

    changes in styles have been made

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Input from "./Input";
 import group from "../Group 48.svg";
-import styles from '../styles/Form.module.css'
+import '../styles/Form.css'
 
 const Form = () => {
     return (
-        <form className={styles.form}>
-            <div className={styles.form__content}>
-                <main className={styles.main__info}>
-                    <h1 className={styles.main__info_title}> Хотите сделать проект с нами?</h1>
-                    <p className={styles.main__info_text}>Поделитесь с нами вашими размышлениями<br/>
+        <form className='form'>
+            <div className='form__content'>
+                <main className='main__info'>
+                    <h1 className='main__info_title'> Хотите сделать проект с нами?</h1>
+                    <p className='main__info_text'>Поделитесь с нами вашими размышлениями<br/>
                         и мы свяжемся с Вами, чтобы реализовать Вашу задумку.</p>
                     <article>
-                        <section className={styles.article__inputs_small}>
+                        <section className='article__inputs_small'>
                             <Input text='Email address*' type='email' size='small'/>
                             <Input text='Ваше имя*' type='text' size='small'/>
                         </section>
@@ -21,9 +21,9 @@ const Form = () => {
                         </section>
                     </article>
                 </main>
-                <img src={group} alt={'logo'} className={styles.logo}/>
+                <img src={group} alt={'logo'} className='logo'/>
             </div>
-            <button className={styles.button}>Отправить</button>
+            <button className='button'>Отправить</button>
         </form>
     );
 };
